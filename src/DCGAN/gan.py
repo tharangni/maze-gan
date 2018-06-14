a9f23992371070baaa5b1236897f98d07095ced9
ManSchri <manon.schriever@student.uva.nl>
2018-06-14 14:08:59 +0200

    DCGAN implementation

---
+++ src/DCGAN/gan.py
@@ -0,0 +1,117 @@
+import os
+import pickle
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from generator import Generator
+from discriminator import Discriminator
+from maze_gen import gen_maze_data
+
+# Device configuration
+class GAN:
+
+    def __init__(self,
+                 device,
+                 input_size,
+                 hidden_size,
+                 num_epochs,
+                 batch_size,
+                 mx,
+                 my,
+                 N,
+                 maze_dir):
+        self.device = device
+        self.G = Generator(self.device, input_size, hidden_size, mx, my, num_epochs, batch_size)
+        self.D = Discriminator(self.device, hidden_size, mx, my, num_epochs, batch_size)
+        self.hidden_size = hidden_size
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.mx = mx
+        self.my = my
+        self.N = N
+        self.maze_dir = maze_dir
+        self.model_dir = "models"
+
+    def denorm(self, x):
+        out = (x + 1) / 2
+        return out.clamp(0, 1)
+
+    def reset_grad(self):
+        self.D.optimizer.zero_grad()
+        self.G.optimizer.zero_grad()
+        
+
+    def train(self):
+
+        #self.transform = transforms.Compose([transforms.ToTensor()])
+        #generate maze data
+        #maze_data = self.transform(gen_maze_data(self.N, self.mx, self.my))
+        maze_data = gen_maze_data(self.N, self.mx, self.my)
+
+        # Data loader
+        data_loader = torch.utils.data.DataLoader(dataset=maze_data,
+                                                  batch_size=self.batch_size,
+                                                  shuffle=True)
+
+        #Creates a criterion that measures the Binary Cross Entropy between the target and the output
+        loss_criterion = nn.BCELoss()
+        total_step = len(data_loader)
+        
+
+        #Start training
+        for epoch in range(self.num_epochs):
+            for local_batch , maze_set in enumerate(data_loader):
+                #maze_set= maze_set.reshape(self.batch_size, -1).to(self.device).float()
+                maze_set = maze_set.to(self.device).float()
+                # l + torch.randn(1, 10)*(r-l) - USING SOFT LABELS INSTEAD OF HARD
+                # Real: 0.0 - 0.1
+                # Fake: 0.9 - 1.0
+                # adding 10% noise to training (i.e. add 10% fake labels to real and vice versa)
+                real_labels = 0 + torch.rand([self.batch_size,1], dtype = torch.float).to(self.device)*(0.1 - 0.0)
+                fake_labels = 0.9 + torch.rand([self.batch_size,1], dtype = torch.float).to(self.device)*(1.0 - 0.9)
+
+                noise_samples = 20
+
+                if(epoch % noise_samples==0):
+                    real_labels = 0.9 + torch.randn([self.batch_size,1], dtype = torch.float).to(self.device)*(1.0 - 0.9)
+                    fake_labels = 0 + torch.randn([self.batch_size,1], dtype = torch.float).to(self.device)*(0.1 - 0.0)
+
+                #Train Discrimator
+                d_loss, fake_score, real_score, fake_mazes = self.D.train(self.G,
+                                                                            self.G.input_size,
+                                                                            maze_set,
+                                                                            loss_criterion,
+                                                                            real_labels,
+                                                                            fake_labels,
+                                                                            self.reset_grad)
+                # d_loss = self.D.backprop( d_loss_fake, d_loss_real, self.reset_grad)
+
+                #Train Generator
+                g_loss = self.G.train(self.D, loss_criterion, real_labels, self.reset_grad)
+                
+                # self.G.backprop(g_loss, self.reset_grad)
+
+                if (local_batch + 1) % 100 == 0 or (epoch + 1) % 100 == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}'
+                          .format(epoch, self.num_epochs, local_batch + 1, total_step, d_loss.item(), g_loss.item(),
+                                  real_score.mean().item(), fake_score.mean().item()))
+
+            # Save real mazes
+            if (epoch + 1) == 1:
+                #maze_set = maze_set.reshape(maze_set.size(0), self.mx, self.my)
+                dump_file(os.path.join(self.maze_dir, 'real_mazes.pickle'), maze_set)
+
+            # Save sampled mazes
+            #fake_mazes = fake_mazes.reshape(fake_mazes.size(0), self.mx, self.my)
+            dump_file(os.path.join(self.maze_dir, 'fake_mazes-{}.pickle'.format(epoch + 1)), fake_mazes)
+
+        if not os.path.exists(self.model_dir):
+            os.makedirs(self.model_dir)
+        torch.save(self.G.model.state_dict(), self.model_dir + '/G.ckpt')
+        torch.save(self.D.model.state_dict(), self.model_dir + '/D.ckpt')
+
+
+def dump_file(loc, data):
+    output = open(loc, 'wb')
+    pickle.dump(data, output)
+    output.close()
---
+++ src/DCGAN/start.py
@@ -0,0 +1,108 @@
+import argparse
+import os
+import torch
+import pickle
+from maze_gen import check_maze, draw_maze
+from gan import GAN
+import numpy as np
+from matplotlib import pyplot as plt
+
+def_dir='maze_results'
+
+
+def visualise_results(dir, eg_no):
+    # path = os.path.join(dir, 'real_mazes.pickle')
+    path = os.path.join(dir, 'fake_mazes-{}.pickle'.format(eg_no))
+    print('Visualising sample from {}'.format(path))
+    #visualise sample from final results
+    mazes = pickle.load(open(path, 'rb'))
+    #print(mazes)
+    #takes sample and plot
+    for maze in mazes[:10]:
+        maze = maze.squeeze()
+        maze[maze<0.5] = 0
+        maze[maze>0.5] = 1
+        # is it a valid maze?
+        if torch.cuda.is_available(): maze = maze.cpu()
+        maze = maze.detach().numpy()
+        check = check_maze(maze)
+        if check:
+            print(check)
+            draw_maze(maze)
+        else:
+            print(check)
+            draw_maze(maze)
+    correct = 0
+    for maze in mazes:
+        maze = maze.squeeze()
+        maze[maze<0.5] = 0
+        maze[maze>0.5] = 1
+        if torch.cuda.is_available(): maze = maze.cpu()
+        maze = maze.detach().numpy()
+        check = check_maze(maze)
+        if check:
+            correct += 1
+            draw_maze(maze)
+    print(correct, ' correct out of ', len(mazes))
+
+
+def test_results(dir, eg_no):
+    path =  os.path.join(dir, 'fake_mazes-{}.pickle'.format(eg_no))
+    print('Testing results from {}'.format(path) )
+    mazes = pickle.load(open(path, 'rb'))
+    # print(mazes)
+    r = []
+    for each_maze in mazes:
+      r.append(check_maze(each_maze))
+    print(len(r))
+    
+
+
+
+def start():
+    #look for cmd arguments here
+
+    parser = argparse.ArgumentParser(description='Run GAN or visualise maze.')
+    parser.add_argument('--v', '--visualise', action='store',nargs=2, help='Visualise a sample of fake results')
+    parser.add_argument('--t', '--test', action='store', nargs=2, help='Test fake results')
+    #------ Have to check which are rows and columns -------#
+    parser.add_argument('--mx', help='No. columns in maze', type=int, default=16)
+    parser.add_argument('--my', help='No. rows in maze', type=int, default=16)
+    parser.add_argument('--N', help='No. of traning examples to generate', type=int, default=200)
+    #-------------------------------------------------------#
+    parser.add_argument('--input_size', help='No. inputs for generator', type=int, default=4)
+    parser.add_argument('--hidden_size', help='No. of hidden neurons', type=int, default=8)
+    parser.add_argument('--num_epochs', help='No. of epochs', type=int, default=200) #i.e. number of fake mazes to generate
+    parser.add_argument('--batch_size', help='Size of batch to use (Must be compatible with N)', type=int, default=100)
+    parser.add_argument('--maze_dir', help='Directory results are stored in', type=str, default=def_dir)
+
+    #TODO check batch size is appropoate for the N given
+
+    args = parser.parse_args()
+
+    if args.v:
+        visualise_results(args.v[0], args.v[1])
+    elif args.t:
+        test_results(args.t[0], args.t[1])
+    else:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        if not os.path.exists(args.maze_dir):
+            os.makedirs(args.maze_dir)
+
+        gan = GAN(device,
+                  args.input_size,
+                  args.hidden_size,
+                  args.num_epochs,
+                  args.batch_size,
+                  args.mx,
+                  args.my,
+                  args.N,
+                  args.maze_dir)
+
+        #train
+        gan.train()
+
+    #save gan
+if __name__ == '__main__':
+    start()
