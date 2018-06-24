e7dd2fe2f2f4b8acd0b1222f733946cf0232c1bd
andrei sili <andrei.sili1994@gmail.com>
2018-06-24 15:58:58 +0200

    added train, eval and misc entry points.

---
+++ src/train.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import importlib
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # -- MODEL OPTIONS -- #
+    parser.add_argument('-m', '--model', type=str, help='the model to use. should reference folder and python file')
+    parser.add_argument('-d', '--dataset', type=str, help='the data set to use. possible values: mnist, mazes')
+    parser.add_argument('-r', '--resume', type=bool, help='whether to resume training')
+
+    # -- LOGGING OPTIONS -- #
+    parser.add_argument('-l', '--log_details', type=bool, help='whether to log parameter and gradient data')
+
+    # -- HYPER PARAMS -- #
+    parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+    parser.add_argument('--n_examples', type=int, default=60000, help='training examples to load (used only for mazes)')
+    parser.add_argument('--batch_size', type=int, default=60, help='size of the batches')
+    parser.add_argument('--d_lr', type=float, default=0.0002, help='adam: learning rate for discriminator')
+    parser.add_argument('--g_lr', type=float, default=0.0002, help='adam: learning rate for generator')
+    parser.add_argument('--sample_interval', type=int, default=400, help='interval between image samples')
+    parser.add_argument('--img_size', type=int, default=28,
+                        help='size of each MNIST image dimension (Discriminator input)')
+    parser.add_argument('--maze_size', type=int, default=8,
+                        help='size of each maze dimension (Discriminator input)')
+    parser.add_argument('--latent_dim', type=int, default=100,
+                        help='The dimension of the latent space (Generator input)')
+    opt = parser.parse_args()
+
+    print(opt)
+
+    # model_dir = os.path.abspath(os.path.join('models', opt.dataset, opt.model, opt.model + '.py'))
+
+    model = importlib.import_module('.'.join(['models', opt.dataset, opt.model, opt.model]))
+    model.run(opt)
