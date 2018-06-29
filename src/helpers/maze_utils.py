181e53b5e523f48d917286a89b3729f16bde1c8b
kjod <kieranjamesodriscoll@gmail.com>
2018-06-29 14:40:51 +0200

    Merged ManSchri code in master.

--- src/helpers/maze_utils.py
+++ src/helpers/maze_utils.py
@@ -16,6 +16,41 @@ import os
 ROOT = os.path.abspath(os.path.join(os.getcwd(), '..'))
 
 
+def base_check_maze(maze: np.ndarray) -> bool:
+    """Checks whether the input is a valid maze. Checks the following:
+            * there are no islands (i.e. there are no loops).
+            * all white pixels are connected.
+
+        Args:
+            maze:  The maze to be evaluated
+
+        Returns:
+            Whether the maze is valid or not.
+        """
+    # single connected-component
+    labeled_array, num_features = label(maze)
+
+    npmaze = np.array(maze)
+    mx, my = npmaze.shape
+    if num_features > 1:
+        return False
+    # no loops
+    for i in range(mx - 1):
+        for j in range(my - 1):
+            if maze[i, j] == 1:
+                if maze[i + 1, j] == 1 and maze[i, j + 1] == 1 and maze[i + 1, j + 1] == 1:
+                    return False
+    maze = maze - 1
+    s = [[1, 1, 1],
+         [1, 1, 1],
+         [1, 1, 1]]
+    labeled_array, num_features = label(maze, structure=s)
+    for feat in range(1, num_features + 1):
+        indexes = np.array(np.where(labeled_array == feat))
+        if np.all(indexes[:, :] != 0) and np.all(indexes[0, :] != mx - 1) and np.all(indexes[1, :] != my - 1):
+            return False
+    return True
+
 def gen_maze(mx: int, my: int) -> np.ndarray:
     """Generate a shape (mx, my) maze.  1's represent "hallways" and 0's represent "walls".
     All "hallways" will be connected into a single component with no loops.
@@ -111,32 +146,34 @@ def check_maze(maze: np.ndarray) -> bool:
     """
 
     # single connected-component
-    labeled_array, num_features = label(maze)
-
-    npmaze = np.array(maze)
-    mx, my = npmaze.shape
-
-    if num_features > 1:
+    mx, my = maze.shape
+    stack = [(j, i) for i in range(mx) for j in range(my) if maze[i, j] == 1]
+    if len(stack) == 0:
         return False
-
-    # no loops
-    for i in range(mx - 1):
-        for j in range(my - 1):
-            if maze[i, j] == 1:
-                if maze[i + 1, j] == 1 and maze[i, j + 1] == 1 and maze[i + 1, j + 1] == 1:
-                    return False
-
-    maze = maze - 1
-    s = [[1, 1, 1],
-         [1, 1, 1],
-         [1, 1, 1]]
-    labeled_array, num_features = label(maze, structure=s)
-    for feat in range(1, num_features + 1):
-        indexes = np.array(np.where(labeled_array == feat))
-
-        if np.all(indexes[:, :] != 0) and np.all(indexes[0, :] != mx - 1) and np.all(indexes[1, :] != my - 1):
+    dx = [0, 1, 0, -1];
+    dy = [-1, 0, 1, 0]  # 4 directions to move in the maze
+    while len(stack) > 0:
+        (cx, cy) = stack[-1]
+        nlst = []  # list of available neighbors
+        for i in range(4):
+            nx = cx + dx[i];
+            ny = cy + dy[i]
+            if nx >= 0 and nx < mx and ny >= 0 and ny < my:
+                if maze[ny][nx] == 0:
+                    # of occupied neighbors must be 1
+                    ctr = 0
+                    for j in range(4):
+                        ex = nx + dx[j];
+                        ey = ny + dy[j]
+                        if ex >= 0 and ex < mx and ey >= 0 and ey < my:
+                            if maze[ey][ex] == 1: ctr += 1
+                    if ctr == 1: nlst.append(i)
+        # if 1 or more neighbors available then randomly select one and move
+        if len(nlst) > 0:
             return False
-    return True
+        else:
+            stack.pop()
+    return base_check_maze(maze)
 
 
 def save_grid(mazes: np.ndarray, path: str) -> None:
