731fc289115890761d0b17de70118bf7760ca6e7
kjod <kieranjamesodriscoll@gmail.com>
2018-06-30 14:43:51 +0200

    Fixed bug in eval.py, was still using datasets folder.

--- src/eval.py
+++ src/eval.py
@@ -18,10 +18,9 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dataset', type=str, help='the data set to use. possible values: mnist, mazes', choices=['mnist', 'mazes'])
     parser.add_argument('-a', '--action', type=str, help='what to do. possible values: check, draw', choices=['draw','check_ind','check_avg', "check_and_draw"])
     opt = parser.parse_args()
-    print("opt ", opt)
 
-    module_path = os.path.abspath(os.path.join('models', opt.dataset, opt.model))
-    samples_path = os.path.abspath(os.path.join('models', opt.dataset, opt.model, 'samples', opt.run, '*.sample.tar'))
+    module_path = os.path.abspath(os.path.join('models', opt.model))
+    samples_path = os.path.abspath(os.path.join('models', opt.model, 'samples', opt.run, '*.sample.tar'))
     sample_files = glob.glob(samples_path)
     sample_files.sort()
 
@@ -31,9 +30,12 @@ if __name__ == '__main__':
     if opt.dataset != 'mazes':
         print("Invalid dataset")
     else:
-        if opt.action == 'check_ind':
-            ev.check_ind(sample_files)
-        if opt.action == 'check_avg':
-            ev.check_avg(sample_files)
-        if opt.action == 'check_and_draw':
-            ev.check_and_draw(sample_files)
+        if len(sample_files) > 0:
+            if opt.action == 'check_ind':
+                ev.check_ind(sample_files)
+            if opt.action == 'check_avg':
+                ev.check_avg(sample_files)
+            if opt.action == 'check_and_draw':
+                ev.check_and_draw(sample_files)
+        else:
+            print("No sample files")
